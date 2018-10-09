e3664a69c7516d00d93f6a6e04d29b7aec24c27d
Susheel Nyamala <snyamala@codeaurora.org>
2018-10-09 16:09:23 +0530

    Add NAME to Unique list of apn fields
    As per operator requirement, two apn entries with different carrier name,
    username and password are needed. To fix apn overwrite, add NAME field to
    Unique apn fields list
    
    Change-Id: I8a3cc97bd8ccb0f213b88d4cd797782d75f75acb
    CRs-Fixed: 2331629

--- src/com/android/providers/telephony/TelephonyProvider.java
+++ src/com/android/providers/telephony/TelephonyProvider.java
@@ -240,9 +240,10 @@ public class TelephonyProvider extends ContentProvider
     private boolean mManagedApnEnforced;
 
     static {
-        // Columns not included in UNIQUE constraint: name, current, edited, user, server, password,
+        // Columns not included in UNIQUE constraint: current, edited, user, server, password,
         // authtype, type, protocol, roaming_protocol, sub_id, modem_cognitive, max_conns,
         // wait_time, max_conns_time, mtu, bearer_bitmask, user_visible, network_type_bitmask
+        CARRIERS_UNIQUE_FIELDS_DEFAULTS.put(NAME, "");
         CARRIERS_UNIQUE_FIELDS_DEFAULTS.put(NUMERIC, "");
         CARRIERS_UNIQUE_FIELDS_DEFAULTS.put(MCC, "");
         CARRIERS_UNIQUE_FIELDS_DEFAULTS.put(MNC, "");
@@ -311,7 +312,7 @@ public class TelephonyProvider extends ContentProvider
                 READ_ONLY + " BOOLEAN DEFAULT 0," +
                 // Uniqueness collisions are used to trigger merge code so if a field is listed
                 // here it means we will accept both (user edited + new apn_conf definition)
-                // Columns not included in UNIQUE constraint: name, current, edited,
+                // Columns not included in UNIQUE constraint: current, edited,
                 // user, server, password, authtype, type, sub_id, modem_cognitive, max_conns,
                 // wait_time, max_conns_time, mtu, bearer_bitmask, user_visible,
                 // network_type_bitmask.
